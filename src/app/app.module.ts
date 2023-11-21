145a43862181fd06f91637e4d13ebea31a72f3e4
Sumiya1412 <kondakamarla.sumiya@hcl.com>
2023-11-21 23:56:09 +0530

    Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+const routes : Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    redirectTo:'login',
+    pathMatch:'full'
+  }
+  ];
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    ForgotPasswordComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+// Custom validator function for email format
+function emailValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  const valid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(control.value);
+  return valid ? null : { invalidEmail: true };
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  showPassword: boolean = false;
+
+  constructor(private formBuilder: FormBuilder,
+    private httpClient: HttpClient,
+    private router:Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      usertype: ['', Validators.required],
+      username: ['', [Validators.required, emailValidator]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLoginClick() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      let loginFormData = this.loginForm.getRawValue();
+      console.log('Entered Values Are', loginFormData);
+      //TODO: replace dummy api url with actual url
+      this.httpClient.post('https://localhost:7082/api/Reges/login', loginFormData).subscribe({
+        next: (res) => {
+          // success 
+          if(res){
+            this.router.navigate(['/dashboard']);
+          }
+          console.log('API Success', res);
+        },
+        error: (err) => {
+          //error
+          console.error('API Fail', err);
+        }
+      })
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+}
