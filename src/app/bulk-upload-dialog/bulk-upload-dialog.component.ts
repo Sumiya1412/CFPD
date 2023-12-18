0e25fceb0acbb757927c1212bf90263e71be421b
Surendra Thota <tkbsurendra@gmail.com>
2023-12-19 01:49:22 +0530

    bulk upload changes

--- src/app/bulk-upload-dialog/bulk-upload-dialog.component.ts
+++ src/app/bulk-upload-dialog/bulk-upload-dialog.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
@@ -8,18 +9,43 @@ import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialo
   styleUrls: ['./bulk-upload-dialog.component.css'],
 })
 export class BulkUploadDialogComponent {
-  constructor(public dialog: MatDialog) { }
+  selectedFile: File | null = null;
+  constructor(public dialog: MatDialog,private http: HttpClient) { }
 
   handleFileInput(event: any): void {
     const file = event.target.files[0];
-    // Handle file input as needed
-    console.log('Selected file:', file);
+
+    if (file) {
+      const allowedExtensions = ['.xlsx', '.xls'];
+      const extension = file.name.substring(file.name.lastIndexOf('.'));
+
+      if (allowedExtensions.includes(extension.toLowerCase())) {
+        this.selectedFile = file;
+      } else {
+        console.error('Invalid file type. Please select a valid Excel file.');
+        this.selectedFile = null;
+      }
+    }
   }
 
-  uploadBulkData(): void {
-    // Implement bulk upload logic using the selected file
-    console.log('Bulk data uploaded!');
-    this.closeDialog();
+  uploadBulkData() {
+    if (!this.selectedFile) {
+      console.error('Please select a file');
+      return;
+    }
+
+    const uploadUrl = 'https://localhost:7082/api/ExcelUpload/upload';
+    const formData: FormData = new FormData();
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+
+    this.http.post(uploadUrl, formData)
+      .subscribe(response => {
+        console.log('File uploaded successfully:', response);
+        // Handle success
+      }, error => {
+        console.error('File upload failed:', error);
+        // Handle error
+      });
   }
 
   closeDialog(): void {
